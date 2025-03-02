b9a3376b6651cc0f963ae9d494ef9966945d5e73
jorgeCasalini <joshcasalini@gmail.com>
2025-03-02 18:05:46 -0300

    Update App.test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("Renderização do link para Learn React", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Conversor de temperatura/i);
   expect(linkElement).toBeInTheDocument();
 });
